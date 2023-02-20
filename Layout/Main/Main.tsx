d5c7422e7bb21c9e16d109129cfc71c36dfb8e84
Serafim <serafimkremnev1008@gmail.com>
2023-02-21 00:32:36 +0300

    sidebar adaptive

--- Layout/Main/Main.tsx
+++ Layout/Main/Main.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import styles from './Main.module.css';
 import {MainProps} from "./Main.props";
 import cn from "classnames";
 import Sidebar from "../Sidebar/Sidebar";
+import Sort from './sort.svg';
+
 
 const Main = ({children, className, ...props}: MainProps) => {
+    const [open, setOpen] = useState<boolean>(false);
+
+
+
     return (
         <main className={cn(className, styles.main)}>
-            <Sidebar className={styles.sidebar}/>
+            <Sidebar className={cn(styles.sidebar, {
+                [styles.sidebarOpen]: open
+            })}/>
+            <Sort onClick={() => setOpen(!open)} className={styles.sort}/>
             <div {...props}>
                 {children}
             </div>
