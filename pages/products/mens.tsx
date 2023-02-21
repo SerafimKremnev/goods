7b7061d1db4c087bace5f543f88a676ebc41212f
Serafim <serafimkremnev1008@gmail.com>
2023-02-21 14:48:34 +0300

    mens&womens

--- pages/products/mens.tsx
+++ pages/products/mens.tsx
@@ -6,20 +6,22 @@ import {AllProducts} from "../../interfaces/products.interface";
 import {useDispatch} from "react-redux";
 import {getCategory} from "../../store/categorySlice";
 import {getProducts} from "../../store/productsSlice";
+import {useProductsSelector} from "../../store";
+import ProductList from "../../Components/ProductList/ProductList";
 
 
 const Mens = ({categories, products}: MensProps) => {
     const dispatch = useDispatch();
+    const stateCategory = useProductsSelector((state) => state.products.products);
 
     useEffect(() => {
-        if(products) {
-            dispatch(getProducts(products));
-        }
         dispatch(getCategory(categories));
+        products && dispatch(getProducts(products));
     }, []);
+
     return (
         <>
-            Mens
+            {stateCategory && <ProductList products={stateCategory}/>}
         </>
     );
 };
@@ -28,7 +30,10 @@ export default withLayout(Mens);
 
 export const getStaticProps: GetStaticProps<MensProps> = async () => {
     const {data: categories} = await axios.get<string[]>(process.env.NEXT_PUBLIC_DOMAIN + '/products/categories');
-    const {data: products} = await axios.get<AllProducts>(process.env.NEXT_PUBLIC_DOMAIN + '/products');
+    const {data: products} = await axios.get<AllProducts>(process.env.NEXT_PUBLIC_DOMAIN + '/products?limit=100');
+    if(products.products) {
+        products.products = products.products.filter(p => p.category.toLowerCase().split('-').includes('mens'));
+    }
     return {
         props: {
             products,
--- pages/products/womens.tsx
+++ pages/products/womens.tsx
@@ -6,18 +6,22 @@ import {AllProducts} from "../../interfaces/products.interface";
 import {useDispatch} from "react-redux";
 import {getCategory} from "../../store/categorySlice";
 import {getProducts} from "../../store/productsSlice";
+import {useProductsSelector} from "../../store";
+import ProductList from "../../Components/ProductList/ProductList";
 
 
 const Womens = ({categories, products}: WomenProps) => {
     const dispatch = useDispatch();
+    const stateCategory = useProductsSelector((state) => state.products.products);
 
     useEffect(() => {
         dispatch(getCategory(categories));
         products && dispatch(getProducts(products));
     }, []);
+
     return (
         <>
-            Womens
+            {stateCategory && <ProductList products={stateCategory}/>}
         </>
     );
 };
@@ -26,7 +30,10 @@ export default withLayout(Womens);
 
 export const getStaticProps: GetStaticProps<WomenProps> = async () => {
     const {data: categories} = await axios.get<string[]>(process.env.NEXT_PUBLIC_DOMAIN + '/products/categories');
-    const {data: products} = await axios.get<AllProducts>(process.env.NEXT_PUBLIC_DOMAIN + '/products');
+    const {data: products} = await axios.get<AllProducts>(process.env.NEXT_PUBLIC_DOMAIN + '/products?limit=100');
+    if(products.products) {
+        products.products = products.products.filter(p => p.category.toLowerCase().includes('womens'));
+    }
     return {
         props: {
             products,
